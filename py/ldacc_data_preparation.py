44fb1c464d5891ecb10864c583f43a7a8bf3049b
RobotFlying <robotflying777@gmail.com>
2015-01-01 15:54:25 +0800

    set input & output of ldacc_data_preparation.py

--- py/ldacc_data_preparation.py
+++ py/ldacc_data_preparation.py
@@ -1,26 +1,27 @@
 #! /usr/bin/python3
-#! -*- encoding:utf-8 -*- 
+#! -*- encoding:utf-8 -*-
 
 import os
+import sys
 from ldacc_Chinese_analyser import analyze
 from ldacc_stopwords_filter import Filter
 
 CORPUS_PATH = "../data/corpus2"
-RESULT_FILE = "../data/document.dat" 
+RESULT_FILE = "../data/document.dat"
 
 
 def write_data(output_file, data):
     for word in data:
         #print(data, end = " ")
-        print(word, end = " ", file = output_file)
+        print(word, end=" ", file=output_file)
     print(file = output_file)
 
-def walk_dir(dir):
-    ofile = open(RESULT_FILE, 'w', encoding = "utf-8")
+def walk_dir(input_dir, output_file):
+    ofile = open(output_file, 'w', encoding = "utf-8")
     fter = Filter()
 
-    for i in os.listdir(dir):
-        input_file = os.path.join(dir, i)
+    for i in os.listdir(input_dir):
+        input_file = os.path.join(input_dir, i)
         print(input_file)
         result = fter.filter(analyze(input_file))
         write_data(ofile, result)
@@ -28,4 +29,4 @@ def walk_dir(dir):
     ofile.close()
 
 if "__main__" == __name__ :
-    walk_dir(CORPUS_PATH)
+    walk_dir(sys.argv[1], sys.argv[2])
